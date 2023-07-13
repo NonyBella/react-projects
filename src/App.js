eefc8d0d710882e99fb34afb10d4e939990db04e
NonyBella <miriam.odeny@gmail.com>
2023-07-13 10:43:09 +0100

    modified code

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <main>
       <section className="container">
-        <h3> {people.length} birthdays</h3>
+        <h3> {people.length} birthdays today</h3>
         <List people={people} />
-        <button onclick={() => console.log("you clicked me")}>clear all</button>
+        <button onClick={() =>setPeople([])}>clear all</button>
       </section>
     </main>
   );
